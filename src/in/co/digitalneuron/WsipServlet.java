cc32835f406ca3543b5e29f9b40afd5035dbb774
sunil <spsarolkar@gmail.com>
2011-01-27 14:55:42 +0530

    initial

---
+++ src/in/co/digitalneuron/WsipServlet.java
@@ -0,0 +1,13 @@
+package in.co.digitalneuron;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class WsipServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
